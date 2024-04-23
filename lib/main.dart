997cdfdf7e0eafd93c3bae01bf312fd2789567b8
AhmedKarray1 <karray.ahmed01@gmaiL.com>
2024-04-23 17:28:24 +0100

    fix code

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,56 @@
 import "package:flutter/material.dart";
-import "package:ppp/my_app.dart";
+import "package:ppp/data/questions.dart";
+
+import "home_screen.dart";
+import "questions_screen.dart";
+import "results_screen.dart";
 
 void main() {
   runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  List<String> selectedAnswers = [];
+  var activeScreen = "home-screen";
+
+  void switchScreen(String screen) {
+    setState(() {
+      activeScreen = screen;
+      if (screen == "home-screen") selectedAnswers = [];
+    });
+  }
+
+  void chooseAnswer(String answer) {
+    selectedAnswers.add(answer);
+    if (selectedAnswers.length == questions.length) {
+      setState(() {
+        activeScreen = "results-screen";
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            color: Color.fromARGB(255, 64, 11, 113),
+          ),
+          child: activeScreen == "home-screen"
+              ? HomeScreen(switchScreen)
+              : activeScreen == "results-screen"
+                  ? ResultsScreen(switchScreen, selectedAnswers)
+                  : QuestionsScreen(chooseAnswer),
+        ),
+      ),
+    );
+  }
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:ppp/my_app.dart';
+import 'package:ppp/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
