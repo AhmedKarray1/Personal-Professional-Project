3011138c2d7a02b788d5c04eeb4a508711d5d546
AhmedKarray1 <karray.ahmed01@gmaiL.com>
2024-04-23 18:17:12 +0100

    start implementing widget testing

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,29 +1,30 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:ppp/answer_button.dart';
 import 'package:ppp/main.dart';
+import 'package:ppp/results_screen.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
+  testWidgets('navigate to questions screen', (WidgetTester tester) async {
     await tester.pumpWidget(const MyApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    expect(find.text('Learn Flutter the fun way!'), findsOneWidget);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
+    await tester.tap(find.byIcon(Icons.arrow_right_alt));
     await tester.pump();
+    expect(find.text('Learn Flutter the fun way!'), findsNothing);
+    expect(
+      find.byType(Column),
+      findsOneWidget,
+    );
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    await tester.tap(find.byWidget(AnswerButton("answerText", () {})));
+    // await tester.tap(find.byType(ElevatedButton));
+    // await tester.tap(find.byType(ElevatedButton));
+    // await tester.tap(find.byType(ElevatedButton));
+    // await tester.tap(find.byType(ElevatedButton));
+    // await tester.tap(find.byType(ElevatedButton));
+
+    // expect(find.text('Restart Quiz!'), findsOneWidget);
   });
 }
